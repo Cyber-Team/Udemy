3bb1661a417a3738442027b95c7390778cf27be6
krishnaUIDev <krishna.kondoju007@gmail.com>
2017-11-01 23:09:50 -0500

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,8 @@ import {RouterModule, Routes} from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule, MatMenuModule
-, MatTooltipModule,
-  MatDialogModule} from '@angular/material';
-import {MatIconModule} from '@angular/material';
+
+
 
 const appRoute: Routes = [
 ];
@@ -21,16 +19,11 @@ const appRoute: Routes = [
   ],
   imports: [
     BrowserModule, BrowserAnimationsModule, HttpModule, RouterModule.forRoot(appRoute, { useHash: true}), FormsModule,
-    MdButtonModule,
-    MdCardModule,
-    MdMenuModule,
-    MdToolbarModule,
-    MdIconModule,
-    MatIconModule,
-    MatMenuModule,
-    MatTooltipModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
