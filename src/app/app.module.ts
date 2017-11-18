7b8f9208fab4ae0900b208a4a8c9b7ba6df20540
krishnaUIDev <krishna.kondoju007@gmail.com>
2017-11-18 17:14:49 -0600

    module changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,11 +14,14 @@ import {MatIconModule} from '@angular/material';
 import {MatChipsModule} from '@angular/material';
 import {MatInputModule} from '@angular/material';
 import {MatCardModule} from '@angular/material';
+import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+/*environment setup*/
+import {environment} from "../environments/environment";
 
 /*angular firebase and auth module */
 
-import {AngularFireDatabaseModule} from "angularfire2/database";
-import {AngularFireAuthModule} from "angularfire2/auth";
+
 /*
 import { Observable } from 'rxjs/Observable';
 import { AngularFireModule } from 'angularfire2';
@@ -27,7 +30,7 @@ import { AngularFireAuthModule} from "angularfire2/auth";
 */
 
 /*components*/
-import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
@@ -39,8 +42,8 @@ import { SignupComponent } from './signup/signup.component';
 import { SigninComponent } from './signin/signin.component';
 import { ForgetComponent } from './forget/forget.component';
 import { TeachComponent } from './teach/teach.component';
-import {DataService} from "./services/data.service";
-import {AuthService} from "./services/Auth/auth.service";
+import { DataService } from './services/data.service';
+import { AuthService } from './services/Auth/auth.service';
 
 @NgModule({
   declarations: [
@@ -59,18 +62,12 @@ import {AuthService} from "./services/Auth/auth.service";
   ],
   imports: [
     BrowserModule,
-    MatButtonModule,
-    MatMenuModule,
-    MatChipsModule,
-    MatInputModule,
-    MatIconModule,
-    MatCardModule,
+    MatButtonModule, MatMenuModule, MatChipsModule, MatInputModule, MatIconModule, MatCardModule,
     BrowserAnimationsModule,
     HttpModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,AngularFireDatabaseModule,
-    AngularFireAuthModule
+    FormsModule, ReactiveFormsModule
+
   ],
   providers: [DataService,AuthService],
   bootstrap: [AppComponent]
