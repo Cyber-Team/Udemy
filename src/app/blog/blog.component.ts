011f1bed6214c290d4103075e685d2ad0508a15b
krishnaUIDev <krishna.kondoju007@gmail.com>
2017-11-25 17:12:37 -0600

    comment updated

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -9,15 +9,28 @@ import {Http} from '@angular/http';
 })
 export class BlogComponent implements OnInit {
   myBlog:any;
-  itemCount:number = 4;
+  itemCount:number;
   btnText: string = "Add";
   goatText: string = "";
-
+  goatName: string = "";
+  goals = [];
+  name = [];
+  goatImg: string;
   constructor(private http: Http) { }
 
   ngOnInit() {
     this.http.get('../assets/json/blog.json')
       .map(response => response.json())
       .subscribe(res => this.myBlog = res);
+
+    this.itemCount = this.goals.length;
+  }
+  addComment(){
+      this.goals.push(this.goatText);
+      this.goatText='';
+      this.itemCount = this.goals.length;
+      this.name.push(this.goatName);
+      this.goatName='';
+      // console.log(this.goatImg);
   }
 }
