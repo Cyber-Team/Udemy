86d19734dcdc8f16a176068042d236b4a9d41fc2
krishnaUIDev <krishna.kondoju007@gmail.com>
2017-12-03 00:02:53 -0600

    sign in changed

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
+  data: any = [];
+  name: string;
+  email: string;
+  pass: any;
+  wrong: string = "";
   constructor() { }
 
   ngOnInit() {
   }
-
+  formSubmi(){
+    // console.log(this.data.name);
+    // console.log(this.data.email);
+    if(this.data.name == "" || this.data.email == "" || this.data.pass == ""){
+       console.log("success");
+    }else{
+      this.wrong = "*Please enter correct input";
+    }
+  }
 }
