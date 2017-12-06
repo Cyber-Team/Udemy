6d8947628ab92d95e3b2f7ba0e44a8c5b478089a
krishnaUIDev <krishna.kondoju007@gmail.com>
2017-12-06 01:10:36 -0600

    service updated

--- src/app/teach/employee-list/employee-list.component.ts
+++ src/app/teach/employee-list/employee-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
 
+
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
@@ -9,9 +10,15 @@ export class EmployeeListComponent implements OnInit {
 
  /* @Input() reviews: string;*/
  @Input() model2: any={};
+/* @Output() notify: EventEmitter<string> = new EventEmitter<string>();*/
+
+  /*updateEmployee(): void{
+    this.notify.emit('msg');
+  }*/
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
--- src/app/teach/employee/employee.component.ts
+++ src/app/teach/employee/employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { NgForm } from '@angular/forms'
+
+
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
@@ -18,7 +19,7 @@ export class EmployeeComponent implements OnInit {
    msg:any="";
    msgg:any="";
 
-  // Input() review:string;
+
    constructor() { }
 
   ngOnInit() {
