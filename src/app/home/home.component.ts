4083481e6065f602b3f8585fa99d1c031a8bd6d6
krishnaUIDev <krishna.kondoju007@gmail.com>
2017-12-11 21:27:15 -0600

    animation started

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, trigger, state, style, transition, animate, keyframes } from '@angular/core';
 import {Http} from '@angular/http';
 import {DataService} from '../services/data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('myTrigger',[
+          state('small', style({
+            transform: 'scale(1)'
+          })),
+          state('large', style({
+            transform: 'scale(1.4)'
+          })),
+        //  transition('small => large', animate('500ms ease-in')),
+      // transition('large => small', animate('500ms ease-out'))
+      transition('small <=> large', animate('500ms'))
+        ])
+  ]
 })
 export class HomeComponent implements OnInit {
   myLi: any;
@@ -13,12 +26,16 @@ export class HomeComponent implements OnInit {
   error: string;
   url: string;
 
+  state: string = 'small';
   constructor(private http: Http) { }
 
   ngOnInit() {
     this.http.get('../assets/json/home.json')
       .map(response => response.json())
       .subscribe(res => this.myLi = res);
+  }
+  toggleState(){
+    this.state = (this.state === 'small' ? 'large' : 'small');
   }
 
 }
